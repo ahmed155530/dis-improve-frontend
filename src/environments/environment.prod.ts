e56dfc6d4a9665dadb55d08c58c5c997380047bc
Ahmed Ammar <Ahmed.Ammar@IT-AHMEDAMMAR>
2024-07-17 12:46:33 +0300

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,4 @@
 export const environment = {
-  //BaseURL: 'http://benaa-dwss.com/api',
-  //BaseURL: 'http://92.205.163.49:8101/api',
-  BaseURL: 'http://92.205.107.6:5071/api',
+  BaseURL: 'dis-improve-1.runasp.net/api',
   production: true
 };
